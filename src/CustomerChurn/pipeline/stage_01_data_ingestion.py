9ca70331e67ba61480ed7d32c0797bea418d2a48
Izam Mohammed <izamdeveloper1@gmail.com>
2023-12-15 11:12:33 +0530

    completed the data ingestion in modular coding

---
+++ src/CustomerChurn/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,38 @@
+from CustomerChurn.config.configuration import ConfigurationManager
+from CustomerChurn.components.data_ingestion import DataIngestion
+from CustomerChurn import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        """
+        Initialize DataIngestionTrainingPipeline instance.
+        """
+        pass
+
+    def main(self):
+        """
+        Execute the main steps of the data ingestion training pipeline.
+
+        Returns:
+            None
+        """
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
