4137d5bcff789721380ae998da9e328327c65922
Izam Mohammed <izamdeveloper1@gmail.com>
2023-12-15 10:52:46 +0530

    edited the common utils

--- src/CustomerChurn/utils/common.py
+++ src/CustomerChurn/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from loanPrediction import logger
+from CustomerChurn import logger
 import json
 import joblib
 from ensure import ensure_annotations
